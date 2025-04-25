80fbfe7fa95f420b25520369fb6ce6155cd5fad8
Professor Isidro <fmassetto@gmail.com>
2025-04-25 12:20:11 -0300

    Add files via upload

---
+++ src/main/java/br/org/pulsemais/actionfigures/ActionfiguresApplication.java
@@ -0,0 +1,13 @@
+package br.org.pulsemais.actionfigures;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActionfiguresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActionfiguresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/org/pulsemais/actionfigures/ActionfiguresApplicationTests.java
@@ -0,0 +1,13 @@
+package br.org.pulsemais.actionfigures;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActionfiguresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
